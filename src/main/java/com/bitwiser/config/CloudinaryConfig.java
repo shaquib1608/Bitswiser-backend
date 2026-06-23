9c5117a7894b921cd72756d8f0c4c51b054f7424
M D SHAQUIB JAHANGIR <117532214+shaquib1608@users.noreply.github.com>
2026-06-23 14:27:58 +0530

    Backend config with env variable

--- src/main/java/com/bitwiser/config/CloudinaryConfig.java
+++ src/main/java/com/bitwiser/config/CloudinaryConfig.java
@@ -2,21 +2,28 @@ package com.bitwiser.config;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CloudinaryConfig {
 
+    @Value("${cloudinary.cloud-name}")
+    private String cloudName;
+    @Value("${cloudinary.api-key}")
+    private String apiKey;
+    @Value("${cloudinary.api-secret}")
+    private String apiSecret;
     @Bean
     public Cloudinary cloudinary() {
-
         return new Cloudinary(
                 ObjectUtils.asMap(
-                        "cloud_name", "dkz3de5jg",
-                        "api_key", "499934813449888",
-                        "api_secret", "LxW_k08t-b9MV1_3yM1UYYIq4Uk"
+                        "cloud_name", cloudName,
+                        "api_key", apiKey,
+                        "api_secret", apiSecret
                 )
         );
     }
+
 }
